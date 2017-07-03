b20ef1f81451736459a2df06e2e965897823ba0a
okeller <orgad.algo2@gmail.com>
2017-07-03 15:02:20 +0300

    _brave.py: added proper work with __future__ palettes.py: added proper work with __future__

--- brave/palettes.py
+++ brave/palettes.py
@@ -1,3 +1,7 @@
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+from builtins import *
+
 entities_palettte = [u'#ff6f66',
                      u'#ff7866',
                      u'#ff8266',
