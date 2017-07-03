30f21ef389b5c47bf6db8190e2be10fe86d2c1ff
Ilana Tuil <ilana.tuil@gmail.com>
2017-07-03 19:00:14 +0300

    change jupyter representation to be without iframe: - change script in embedded_brat template - add _repr_html method to HtmlContrainer

--- brave/_brave.py
+++ brave/_brave.py
@@ -30,14 +30,8 @@ class HtmlContainer(object):
     def __init__(self, html):
         self.html = html
 
-    def save(self, path):
-        with open(path, 'wb') as f:
-            f.write(self.html)
-
-    def to_jupyter(self, width=800, height=600):
-        from IPython.lib.display import IFrame
-        self.save('temp_visual.html')
-        return IFrame('temp_visual.html', width=width, height=height)
+    def _repr_html_(self):
+        return self.html
 
 
 class BraveData(object):
