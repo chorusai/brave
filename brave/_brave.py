3c479ba22451bdf36d3fa4149e6d3df603a5a029
okeller <orgad.algo2@gmail.com>
2017-07-04 14:26:28 +0300

    _brave.py: refactored. brave_entity_relations is now `brave_simple`. Added the `start_notebook_mode` func brave_example.py: fixed main embedded_brat__template.html: was reverted to an earlier version Added tests, and some minor changes

--- brave/_brave.py
+++ brave/_brave.py
@@ -5,26 +5,75 @@ from builtins import *
 import os
 import json
 import logging
+
 from brave.palettes import *
 
 logger = logging.getLogger(__name__)
 
+__mode = 'script'
 
 
-def brave(collData, docData):
-    current_file = os.path.dirname(__file__)
-    parent = os.path.abspath(os.path.join(current_file, os.pardir))
+def start_notebook_mode(in_iframe=True):
+    global __mode
+    if in_iframe:
+        __mode = 'iframe'
+    else:
+        __mode = 'embedded'
+
+
+def save(html, path):
+    with open(path, 'wb') as f:
+        f.write(html)
+
+
+def brave(collData, docData, save_to_path=None, width=800, height=600):
+    parent = os.path.dirname(__file__)
+    parent = os.path.dirname(parent)
     fn = os.path.join(parent, 'templates', 'embedded_brat__template.html')
     template = open(fn).read()
 
     template = template.replace("{0}", json.dumps(collData))
-    template = template.replace("{1}", json.dumps(docData))
-    return HtmlContainer(template)
+    html = template.replace("{1}", json.dumps(docData))
+
+    if save_to_path:
+        save(html, save_to_path)
+
+    if __mode == 'iframe':
+        if save_to_path:
+            eff_path = save_to_path
+        else:
+            eff_path = 'temp_visual.html'
+            save(html, eff_path)
+
+        ret_val = HtmlContainer("""<iframe
+                    width="{width}"
+                    height="{height}"
+                    src="{src}"
+                    frameborder="0"
+                    allowfullscreen
+                ></iframe>
+                """.format(src=eff_path,
+                           width=width,
+                           height=height))
+    elif __mode == 'embedded':
+        raise NotImplementedError(
+            'Pure `embedded` mode is not supported yet. If running in Jupyter, use `iframe` mode.')
+    else:
+        ret_val = html
+    return ret_val
 
 
-def brave_entity_relations(doc_data, coll_data={}):
-    brave_data = BraveData(doc_data, coll_data)
-    return brave(brave_data.doc_data, brave_data.coll_data)
+def brave_simple(doc_data):
+    """
+    This method currently supported only entities and relations!
+    Args:
+        doc_data:
+
+    Returns:
+
+    """
+    brave_data = BraveData(doc_data)
+    return brave(brave_data.coll_data, brave_data.doc_data)
 
 
 class HtmlContainer(object):
@@ -36,9 +85,9 @@ class HtmlContainer(object):
 
 
 class BraveData(object):
-    def __init__(self, doc_data, coll_data={}):
+    def __init__(self, doc_data, coll_data=None):
         self.doc_data = doc_data
-        if len(coll_data) > 0:
+        if coll_data is not None:
             self.coll_data = coll_data
         else:
             self.coll_data = {}
---
+++ tests/basic_test.py
@@ -0,0 +1,20 @@
+from __future__ import (absolute_import, division,
+                        print_function, unicode_literals)
+from builtins import *
+import os
+from unittest import TestCase
+
+
+from brave import brave
+from examples.brave_example import coll_data, doc_data
+
+
+class BasicTest(TestCase):
+    def test_basic(self):
+
+        html = brave(coll_data, doc_data, save_to_path='temp_visual.html').strip()
+
+        baseline = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'examples', 'output_example.html')
+
+        self.assertEqual(html, open(baseline).read().strip())
+        self.assertEqual(html, open('temp_visual.html').read().strip())
