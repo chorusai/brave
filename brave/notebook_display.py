b23518ef9651babb813648bfe33d779506a1ad3e
Ilana Tuil <ilana.tuil@gmail.com>
2017-07-23 15:36:34 +0300

    fix notebook display issues:
    - fix notebook menu buttons on embedded display mode
    - fix js error on open saved notebook

--- brave/notebook_display.py
+++ brave/notebook_display.py
@@ -14,36 +14,12 @@ def try_import_ipython():
 
 def get_init_script():
     return """
-    <script>
-    if(!$('.brave_css').size()){
-        var link = document.createElement("link");
-        link.class = 'brave_css'
-        link.type = "text/css";
-        link.rel = "stylesheet";
-        link.href = "https://cdn.jsdelivr.net/gh/nlplab/brat@v1.3_Crunchy_Frog/style-vis.css";
-        document.getElementsByTagName("head")[0].appendChild(link);
-    }
-    requirejs.config(
-        {
-
-            'paths': {
-                'jquery': 'https://cdn.jsdelivr.net/gh/nlplab/brat@v1.3_Crunchy_Frog/client/lib/jquery.min',
-                'svg': 'https://cdn.jsdelivr.net/gh/nlplab/brat@v1.3_Crunchy_Frog/client/lib/jquery.svg.min',
-                'svgdom': 'https://cdn.jsdelivr.net/gh/nlplab/brat@v1.3_Crunchy_Frog/client/lib/jquery.svgdom.min',
-                'brat_config': 'https://cdn.jsdelivr.net/gh/nlplab/brat@v1.3_Crunchy_Frog/client/src/configuration',
-                'brat_utils': 'https://cdn.jsdelivr.net/gh/nlplab/brat@v1.3_Crunchy_Frog/client/src/util',
-                'brat_annotation': 'https://cdn.jsdelivr.net/gh/nlplab/brat@v1.3_Crunchy_Frog/client/src/annotation_log',
-                'brat_webfont': 'https://cdn.jsdelivr.net/gh/nlplab/brat@v1.3_Crunchy_Frog/client/lib/webfont',
-                'brat_dispatcher': 'https://cdn.jsdelivr.net/gh/nlplab/brat@v1.3_Crunchy_Frog/client/src/dispatcher',
-                'brat_monitor': 'https://cdn.jsdelivr.net/gh/nlplab/brat@v1.3_Crunchy_Frog/client/src/url_monitor',
-                'brat_visualizer': 'https://cdn.jsdelivr.net/gh/nlplab/brat@v1.3_Crunchy_Frog/client/src/visualizer'
-
-            }})
-    if(!window.Util){
-        requirejs(['svg', 'svgdom', 'brat_config', 'brat_annotation', 'brat_webfont', 'brat_dispatcher', 'brat_monitor', 'brat_visualizer', 'brat_utils'])
-    }        
-    </script>
-    """
+    {loadStyles}
+    if(!window.Util){{
+        {requireJsConfig}
+        requirejs(['svg', 'brat_config', 'brat_annotation', 'brat_webfont', 'brat_dispatcher', 'brat_monitor', 'brat_visualizer', 'brat_utils']);
+    }}        
+    """.format(loadStyles=_get_load_styles_js(), requireJsConfig=_get_require_js_config())
 
 
 def get_embedded_html(coll_data, doc_data):
@@ -57,7 +33,48 @@ def get_embedded_html(coll_data, doc_data):
             bratLocation + '/static/fonts/Liberation_Sans-Regular.ttf'
         ];
     var collData = {collData};
-    var docData = {docData};   
-    Util.embed( 'embed_area_{rand}',collData, docData, webFontURLs);
+    var docData = {docData};
+    {loadStyles}
+    if(!window.Util){{ 
+        {requireJsConfig}
+        requirejs(['svg', 'brat_config', 'brat_annotation', 'brat_webfont', 'brat_dispatcher', 'brat_monitor', 'brat_visualizer', 'brat_utils'], 
+            function(){{
+             window.Util.embed( 'embed_area_{rand}',collData, docData, webFontURLs);
+        }});
+     }}
+    else {{ window.Util.embed( 'embed_area_{rand}',collData, docData, webFontURLs); }}
     </script>
-    """.format(collData=coll_data, docData=doc_data, rand=getrandbits(64))
+    """.format(collData=coll_data, docData=doc_data, rand=getrandbits(64), loadStyles=_get_load_styles_js(),
+               requireJsConfig=_get_require_js_config())
+
+
+def _get_load_styles_js():
+    return """
+    if(!$('.brave_css').size()){
+        var link = document.createElement("link");
+        link.class = 'brave_css';
+        link.type = "text/css";
+        link.rel = "stylesheet";
+        link.href = "https://cdn.jsdelivr.net/gh/nlplab/brat@v1.3_Crunchy_Frog/style-vis.css";
+        document.getElementsByTagName("head")[0].appendChild(link);
+    }
+    """
+
+
+def _get_require_js_config():
+    return """
+        requirejs.config(
+            {
+
+                'paths': {
+                    'svg': 'https://cdn.jsdelivr.net/gh/nlplab/brat@v1.3_Crunchy_Frog/client/lib/jquery.svg.min',
+                    'brat_config': 'https://cdn.jsdelivr.net/gh/nlplab/brat@v1.3_Crunchy_Frog/client/src/configuration',
+                    'brat_utils': 'https://cdn.jsdelivr.net/gh/nlplab/brat@v1.3_Crunchy_Frog/client/src/util',
+                    'brat_annotation': 'https://cdn.jsdelivr.net/gh/nlplab/brat@v1.3_Crunchy_Frog/client/src/annotation_log',
+                    'brat_webfont': 'https://cdn.jsdelivr.net/gh/nlplab/brat@v1.3_Crunchy_Frog/client/lib/webfont',
+                    'brat_dispatcher': 'https://cdn.jsdelivr.net/gh/nlplab/brat@v1.3_Crunchy_Frog/client/src/dispatcher',
+                    'brat_monitor': 'https://cdn.jsdelivr.net/gh/nlplab/brat@v1.3_Crunchy_Frog/client/src/url_monitor',
+                    'brat_visualizer': 'https://cdn.jsdelivr.net/gh/nlplab/brat@v1.3_Crunchy_Frog/client/src/visualizer'
+
+                }});
+    """
